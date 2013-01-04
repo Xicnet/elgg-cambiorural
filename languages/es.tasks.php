b87674a20e7cf6ff5c3528559ffafed52634731f
XICnet <code@xicnet.com>
2013-01-04 15:08:54 -0300

    Fix some tasks plugin spanish translations

--- languages/es.tasks.php
+++ languages/es.tasks.php
@@ -7,18 +7,23 @@ $language = array (
   'tasks:add' => 'Añadir tarea',
   'tasks:addlist' => 'Añadir lista',
   'tasks:lists' => 'Lista de tareas',
+
   'tasks:lists:owner' => 'Lista de tareas de %s',
   'tasks:lists:friends' => 'Lista de tareas de amigxs',
   'tasks:lists:all' => 'Todas las listas de tareas del sitio',
+
   'tasks:group' => 'Tareas del grupo',
   'groups:enabletasks' => 'Activa las tareas del grupo',
+
   'tasks:edit' => 'Edita esta tarea',
   'tasks:delete' => 'Borra esta tarea',
   'tasks:view' => 'Ver esta tarea',
+
   'tasks:lists:add' => 'Añade una lista de tareas',
   'tasks:lists:edit' => 'Edita esta lista de tareas',
   'tasks:lists:delete' => 'Borra esta lista de tareas',
   'tasks:lists:view' => 'Ver lista de tareas',
+
   'tasks:via' => 'via tareas',
   'item:object:tasklist_top' => 'Lista de tareas de arriba',
   'item:object:tasklist' => 'Lista de tareas',
@@ -31,12 +36,19 @@ $language = array (
   'tasks:this:moreinfo' => 'Puedes encontrar más info %s.',
   'tasks:this:moreinfo:here' => 'aquí',
   'tasks:this:referer:comment' => 'Nueva tarea: %s.',
+
   'tasks:priority:low' => 'Prioridad baja',
   'tasks:priority:normal' => 'Prioridad normal',
   'tasks:priority:high' => 'Prioridad alta',
+
   'tasks:participant' => '%s colaborando',
   'tasks:participants' => '%s colaboradorxs',
   'tasks:participants:see' => 'Ver quien está colaborando',
+
+  /**
+  * River
+  **/
+
   'river:create:object:task' => '%s ha creado una tarea %s',
   'river:create:object:tasklist' => '%s ha creado una lista de tareas %s',
   'river:create:object:tasklist_top' => '%s ha creado una lista de tareas %s',
@@ -46,6 +58,7 @@ $language = array (
   'river:comment:object:task' => '%s ha comentado la lista de tareas %s',
   'river:comment:object:tasklist' => '%s ha comentado la lista de tareas %s',
   'river:comment:object:tasklist_top' => '%s ha comentado la lista de tareas %s',
+
   'river:assign:object:task' => '%s se ha asignado la tarea %s',
   'river:activate:object:task' => '%s ha marcado como activa la tarea %s',
   'river:assign_and_activate:object:task' => '%s ha marcado como activa la tarea %s y se la ha asignado',
@@ -54,6 +67,11 @@ $language = array (
   'river:reopen:object:task' => '%s ha reabierto la tarea %s',
   'river:leave:object:task' => '%s ha dejado la tarea %s',
   'river:close:object:task' => '%s ha cerrado la tarea %s',
+
+  /**
+   * Form fields
+   */
+
   'tasks:title' => 'Nombre',
   'tasks:description' => 'Descripción',
   'tasks:list_guid' => 'Lista',
@@ -62,9 +80,11 @@ $language = array (
   'tasks:elapsed_time' => 'Tiempo pasado',
   'tasks:remaining_time' => 'Tiempo restante (para finalizar la tarea)',
   'tasks:access_id' => '¿Quién puede ver esta tarea?',
+
   'tasks:changehistory' => 'Historial de cambios',
   'tasks:comments:post' => 'Guardar cambios',
   'tasks:add:gofull' => 'Ver formulario completo',
+
   'tasks:state:actions' => 'Acciones',
   'tasks:state:action:noaction' => 'Marca como <em>%s</em>',
   'tasks:state:action:assign' => 'Aceptar esta tarea',
@@ -75,15 +95,21 @@ $language = array (
   'tasks:state:action:mark_as_done' => 'Marca esta tarea como realizada',
   'tasks:state:action:close' => 'Cierra esta tarea',
   'tasks:state:action:reopen' => 'Reabre esta tarea',
+
   'tasks:assigned' => 'Tareas asignadas',
   'tasks:unassigned' => 'Tareas no asignadas',
   'tasks:closed' => 'Tareas cerradas',
+
   'tasks:lists:title' => 'Nombre',
   'tasks:lists:description' => 'Descripción',
   'tasks:lists:startdate' => 'Fecha de inicio',
   'tasks:enddate' => 'Fecha límite',
   'tasks:lists:tags' => 'Etiquetas',
   'tasks:lists:access_id' => '¿Quién puede ver esta lista de tareas?',
+
+  /**
+   * Status and error messages
+   */
   'tasks:noaccess' => 'No tienes acceso a esta tarea',
   'tasks:cantedit' => 'No puedes editar esta tarea',
   'tasks:saved' => 'Tarea guardada',
@@ -92,6 +118,7 @@ $language = array (
   'tasks:delete:success' => 'La tarea se ha borrado correctamente.',
   'tasks:delete:failure' => 'La tarea no se ha podido borrar.',
   'tasks:status:changed' => 'El estado de esta tarea ha canviado correctamente',
+
   'tasks:lists:noaccess' => 'No tienes acceso a la lista de tareas',
   'tasks:lists:cantedit' => 'No puedes editar esta lista de tareas',
   'tasks:lists:saved' => 'Lista de tareas guardada',
@@ -99,6 +126,10 @@ $language = array (
   'tasks:lists:error:no_title' => 'Debes especificar un título para esta lista de tareas',
   'tasks:lists:delete:success' => 'La lista de tareas se ha borrado correctamente',
   'tasks:lists:delete:failure' => 'La lista de tareas no se ha podido borrar.',
+
+  /**
+   * Task
+   */
   'tasks:strapline:new' => 'Creada %s por %s',
   'tasks:strapline:assigned' => 'Asignada %s por %s',
   'tasks:strapline:unassigned' => 'Abandonada %s por %s',
@@ -106,12 +137,22 @@ $language = array (
   'tasks:strapline:done' => 'Realizada %s por %s',
   'tasks:strapline:closed' => 'Cerrada %s por %s',
   'tasks:strapline:reopened' => 'Reabierta %s por %s',
+
+  /**
+   * Task list
+   */
   'tasks:lists:strapline' => 'Creada %s por %s',
   'tasks:lists:deadline' => 'Deadline %s',
+
   'tasks:lists:graph:total' => '%s tareas',
   'tasks:lists:graph:remaining' => 'Pendientes %s',
   'tasks:lists:graph:assigned' => 'Asignadas %s',
   'tasks:lists:graph:active' => 'Activas %s',
+
+  /**
+   * Change history
+   */
+
   'tasks:history:assign' => 'se ha asignado esta tarea',
   'tasks:history:activate' => 'ha marcado ésta como su tarea activa',
   'tasks:history:deactivate' => 'ha desmarcado ésta como su tarea activa',
@@ -120,24 +161,63 @@ $language = array (
   'tasks:history:reopen' => 'ha reabierto esta tarea',
   'tasks:history:leave' => 'ha abandonado esta tarea',
   'tasks:history:close' => 'ha cerrado esta tarea (lo la ha realizado)',
+
+  /**
+   * Widget
+   **/
+
   'tasks:active' => 'Tareas activas',
   'tasks:num' => 'Número de tareas a mostrar',
   'tasks:widget:description' => 'Esta es una lista de tus tareas.',
+
+  /**
+   * Submenu items
+   */
   'tasks:label:view' => 'Ver tarea',
   'tasks:label:edit' => 'Editar tarea',
+
   'tasks:lists:label:view' => 'Ver lista de tareas',
   'tasks:lists:label:edit' => 'Editar lista de tareas',
+
+  /**
+   * Sidebar items
+   */
   'tasks:sidebar:this' => 'Esta lista',
   'tasks:sidebar:children' => 'lista de tareas',
   'tasks:sidebar:list' => 'Lista',
   'tasks:navigation' => 'Lista de tareas',
+
   'tasks:newchild' => 'Crea una tarea en esta lista',
   'tasks:backtolist' => 'Volver a \'%s\'',
+
+  /**
+   * Times
+   */
   'friendlytime:future:minutes' => 'en %s minutos',
   'friendlytime:future:minutes:singular' => 'en un minuto',
   'friendlytime:future:hours' => 'en %s horas',
   'friendlytime:future:hours:singular' => 'en una hora',
   'friendlytime:future:days' => 'en %s días',
   'friendlytime:future:days:singular' => 'mañana',
+
+  /**
+   * Comments
+   */
+  'tasks:email:subject' => 'Cambio de estado en tarea',
+  'tasks:email:body' => "%s cambió el estado de tu tarea \"%s\" a %s.
+
+
+%s
+
+
+Para responder o ver el item original, clickea aquí:
+
+%s
+
+Para ver el perfil de %s, clickea aquí:
+
+%s
+
+No podés responder a este mensaje.",
 );
 add_translation("es", $language);
